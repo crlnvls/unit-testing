16b6dd6ccaff5fb4cefc7293b756e124311d2656
“crlnvls” <cveloso@hotmail.co.uk>
2022-07-17 12:50:51 +0100

    Group tests together

--- tests/getLargest.test.js
+++ tests/getLargest.test.js
@@ -2,12 +2,25 @@ const getLargest = require("../getLargest");
 
 // console.log(getLargest);
 
-test("getLargest is a function", () => {
-  expect(typeof getLargest).toBe("function");
-});
+// test("getLargest is a function", () => {
+//   expect(typeof getLargest).toBe("function");
+// });
 
-test("getLargest returns the largest value", () => {
-  const result = getLargest(1, 2, 3);
+// test("getLargest returns the largest value", () => {
+//   const result = getLargest(1, 2, 3);
 
-  expect(result).toEqual(3);
+//   expect(result).toEqual(3);
+// });
+
+// To simplify we use "describe" -->
+describe("getLargest", () => {
+  test("It is a function", () => {
+    expect(typeof getLargest).toBe("function");
+  });
+
+  test("It returns the largest value", () => {
+    const result = getLargest(1, 2, 3);
+
+    expect(result).toEqual(3);
+  });
 });
